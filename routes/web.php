6ef02a4d9cf91097c277640ccf2d4d4ae59de59b
DavidMatias44 <daosmaru3667@gmail.com>
2025-07-27 13:10:02 -0600

    Added task routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+        Route::get('create', [TaskController::class, 'create'])->name('tasks.create');
+        Route::post('create', [TaskController::class, 'store'])->name('tasks.store');
+        Route::get('edit/{id}', [TaskController::class, 'edit'])->name('tasks.edit');
+        Route::put('update/{id}', [TaskController::class, 'update'])->name('tasks.update');
+        Route::delete('delete/{id}', [TaskController::class, 'destroy'])->name('tasks.delete');
+
+        Route::get('/{id}', [TaskController::class, 'show'])->name('tasks.show');
+    });
 });
 
 require __DIR__.'/auth.php';
