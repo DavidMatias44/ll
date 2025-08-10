bbc041ad51386ae0373f1cfe70b9550cee3ed6c8
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-10 10:53:39 -0600

    add feature tests for task creation

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,113 @@
+<?php
+
+test('a guest cannot create a task', function () {
+    $response = $this->post('/tasks/create', [
+        'name' => 'Test Task',
+        'description' => 'Test Task description',
+        'state' => App\Enums\State::TODO->value,
+        'priority' => App\Enums\Priority::HIGH->value,
+    ]);
+
+    $response->assertRedirect('/login');
+    $this->assertDatabaseMissing('tasks', [
+        'name' => 'Test Task',
+    ]);
+});
+
+test('a user can create a task', function () {
+    $user = App\Models\User::factory()->create();
+    $this->actingAs($user); // User has to be authenticated to create a new task.
+
+    $response = $this->post('/tasks/create', [
+        'name' => 'Test Task',
+        'description' => 'Test Task description',
+        'state' => App\Enums\State::TODO->value,
+        'priority' => App\Enums\Priority::HIGH->value,
+    ]);
+
+    $response->assertRedirect('/tasks');
+    $this->assertDatabaseHas('tasks', [
+        'name' => 'Test Task',
+        'user_id' => $user->id,
+    ]);
+});
+
+test('a user cannot create a task without name', function () {
+    $user = App\Models\User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+    $this->actingAs($user); // User has to be authenticated to create a new task.
+
+    $response = $this->post('/tasks/create', [
+        'description' => 'Test Task description',
+        'state' => App\Enums\State::TODO->value,
+        'priority' => App\Enums\Priority::HIGH->value,
+    ]);
+
+    $response->assertSessionHasErrors(['name']);
+    $this->assertDatabaseMissing('tasks', [
+        'description' => 'Test Task description',
+    ]);
+});
+
+test('a user cannot create a task without description', function () {
+    $user = App\Models\User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+    $this->actingAs($user); // User has to be authenticated to create a new task.
+
+    $response = $this->post('/tasks/create', [
+        'name' => 'Test Task',
+        'state' => App\Enums\State::TODO->value,
+        'priority' => App\Enums\Priority::HIGH->value,
+    ]);
+
+    $response->assertSessionHasErrors(['description']);
+    $this->assertDatabaseMissing('tasks', [
+        'name' => 'Test Task',
+    ]);
+});
+
+test('a user cannot create a task without state', function () {
+    $user = App\Models\User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+    $this->actingAs($user); // User has to be authenticated to create a new task.
+
+    $response = $this->post('/tasks/create', [
+        'name' => 'Test Task',
+        'description' => 'Test Task description',
+        'priority' => App\Enums\Priority::HIGH->value,
+    ]);
+
+    $response->assertSessionHasErrors(['state']);
+    $this->assertDatabaseMissing('tasks', [
+        'name' => 'Test Task',
+    ]);
+});
+
+test('a user cannot create a task without priority', function () {
+    $user = App\Models\User::factory()->create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+    $this->actingAs($user); // User has to be authenticated to create a new task.
+
+    $response = $this->post('/tasks/create', [
+        'name' => 'Test Task',
+        'description' => 'Test Task description',
+        'state' => App\Enums\State::TODO->value,
+    ]);
+
+    $response->assertSessionHasErrors(['priority']);
+    $this->assertDatabaseMissing('tasks', [
+        'name' => 'Test Task',
+    ]);
+});
