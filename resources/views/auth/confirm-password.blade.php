f614ced82e35c3db0fa8b62cc766cc6f61209c64
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-19 17:46:43 -0600

    revert view styles to Laravel default: auth files

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,19 +1,27 @@
-@extends('layouts.main')
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+    </div>
 
-@push('css-files')
-    <link rel="stylesheet" href="{{ asset('css/forms.css') }}">
-@endpush
+    <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
 
-@section('main')
-    <section class="form-container">
-        <form method="POST" action="{{ route('password.confirm') }}" class="form">
-            @csrf
+        <!-- Password -->
+        <div>
+            <x-input-label for="password" :value="__('Password')" />
 
-            <p class="big-p">Confirm your password before continuing.</p>
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
 
-            <label for="password">Password</label>
-            <input type="password" name="password">
-            <button class="accept-button">Confirm</button>
-        </form>
-    </section>
-@endsection
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <div class="flex justify-end mt-4">
+            <x-primary-button>
+                {{ __('Confirm') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
