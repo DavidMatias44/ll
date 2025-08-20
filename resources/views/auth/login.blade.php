c1e2e6466ea4e963f61a0ec2feb3a813e91be93e
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-20 17:31:45 -0600

    fix: 'Create an account' anchor does not have proper href

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-6">
-            <a class="underline text-md text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Create an account</a>
+            <a class="underline text-md text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">Create an account</a>
         </div>
     </form>
 </x-guest-layout>
