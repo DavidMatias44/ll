98633f7f6a946bdf77fe7c302d832680175b6c99
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-23 09:26:58 -0600

    fix: welcome message now changes if the user is authenticated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Welcome! Please login.') }}
+            @auth
+                {{ __('You can see your tasks ') }} <a class="underline" href="{{ route('tasks.index') }}">here</a>.
+            @endauth
+
+            @guest
+                {{ __('Welcome! Please login.') }}                
+            @endguest
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
-                    {{ __("If you do not have an account you can create one") }} <a class="underline" href="{{ route('register') }}">here</a>.
-                </div>
+                @auth
+                    <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
+                        {{ __("Welcome!") }} {{ Auth::user()->name }}
+                    </div>   
+                @endauth
+
+                @guest
+                    <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
+                        {{ __("If you do not have an account you can create one") }} <a class="underline" href="{{ route('register') }}">here</a>.
+                    </div>    
+                @endguest
             </div>
         </div>
     </div>
