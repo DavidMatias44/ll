28e64949ef55f133068c43283673d03af6da9c8a
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-25 11:01:59 -0600

    refactor: reimplement pomodoro feature in AlpineJS

--- resources/views/pomodoro.blade.php
+++ resources/views/pomodoro.blade.php
@@ -2,11 +2,11 @@
     <div class="py-12">
         <div class="w-1/2 mx-auto sm:px-6 lg:px-8">
             <div class="px-6 py-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div x-data="{ timerIsRunning : false }" id="pomodoro-timer" class="w-3/4 flex flex-col items-center mx-auto my-12 py-6 text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-800 rounded">
+                <div x-data="pomodoroTimer" id="pomodoro-timer" class="w-3/4 flex flex-col items-center mx-auto my-12 py-6 text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-800 rounded">
                     <div class="flex flex-row justify-start w-full mb-6">
                         <div class="w-[12.5%]"></div>
-                        <p id="pomodoro-timer-text" class="w-3/4 h-full dark:bg-gray-700 text-9xl text-center py-6 rounded"></p>
-                        <button @click="timerIsRunning = false" id="skip-to-next-pomodoro-button" x-bind:class="{ 'hidden' : !timerIsRunning }" class="w-[12.5%] flex items-center justify-center">
+                        <p x-text="getTimeFormat" class="w-3/4 h-full dark:bg-gray-700 text-9xl text-center py-6 rounded"></p>
+                        <button x-show="timerIsRunning" @click="skipToNextPomodoro()" class="w-[12.5%] flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                             </svg>
@@ -14,11 +14,12 @@
                     </div>
 
                     <div class="text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-800 rounded">
-                        <p id="current-pomodoro-text" class="text-2xl"></p>
+                        <p class="text-2xl inline">Pomodoro: #</p>
+                        <p x-text="currentPomodoro" class="text-2xl inline"></p>
                     </div>
 
-                    <button x-bind:disabled="timerIsRunning" @click="timerIsRunning = true" type="button" id="start-pomodoro-timer-button" class="text-xl w-1/4 mt-6 bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-2 px-4 rounded disabled:bg-gray-700 disabled:cursor-not-allowed">Start timer</button>
-                    <button x-bind:disabled="!timerIsRunning" @click="timerIsRunning = false" type="button" id="pause-pomodoro-timer-button" class="text-xl w-1/4 mt-6 bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-2 px-4 rounded disabled:bg-gray-700 disabled:cursor-not-allowed">Pause timer</button>
+                    <button x-show="!timerIsRunning" @click="pomodoroTimerStart()" type="button" id="start-pomodoro-timer-button" class="text-xl w-1/4 mt-6 bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-2 px-4 rounded disabled:bg-gray-700 disabled:cursor-not-allowed">Start timer</button>
+                    <button x-show="timerIsRunning" @click="pomodoroTimerPause()" type="button" id="pause-pomodoro-timer-button" class="text-xl w-1/4 mt-6 bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-2 px-4 rounded disabled:bg-gray-700 disabled:cursor-not-allowed">Pause timer</button>
                 </div>
             </div>
         </div>
@@ -26,75 +27,61 @@
 </x-app-layout>
 
 <script>
-    let pomodoroTimer = document.getElementById('pomodoro-timer');
-    let pomodoroTimerText = document.getElementById('pomodoro-timer-text');
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('pomodoroTimer', () => ({
+            intervalId: null,
+            currentPomodoro: 1,
+            totalTimeInSeconds: 3,
+            timeLeftInSeconds: null,
+            timerIsRunning: false,
 
-    let startPomodoroTimerButton = document.getElementById('start-pomodoro-timer-button');
-    let pausePomodoroTimerButton = document.getElementById('pause-pomodoro-timer-button');
-    let skipToNextPomodoroButton = document.getElementById('skip-to-next-pomodoro-button');
+            init() {
+                this.setUpPomodoroTimer();
+            },
 
-    let currentPomodoroText = document.getElementById('current-pomodoro-text');
-    let currentPomodoro = 1;
+            setUpPomodoroTimer() {
+                this.timeLeftInSeconds = this.totalTimeInSeconds;
+                this.timerIsRunning = false;
+            },
 
-    let timerInterval;
+            pomodoroTimerStart() {
+                this.timerIsRunning = true;
 
-    let totalTimeInSeconds = 0.25 * 60; // this '25' may vary, that depends on user preferences.
-    let timeLeftInSeconds;
+                this.intervalId = setInterval(() => {
+                    if (this.timeLeftInSeconds <= 0) {
+                        this.skipToNextPomodoro();
+                    } else {
+                        this.timeLeftInSeconds--;
+                    }
+                }, 1000)
+            },
 
-    (() => {
-        setupTimer();
-    })();
+            pomodoroTimerPause() {
+                clearInterval(this.intervalId);
+                this.intervalId = null;
+            },
 
-    function setupTimer () {
-        timeLeftInSeconds = totalTimeInSeconds;
-        pomodoroTimerText.textContent = getTimeFormat(timeLeftInSeconds);
-        currentPomodoroText.textContent = "Pomodoro: #" + currentPomodoro.toString();
-    }
+            skipToNextPomodoro() {
+                this.currentPomodoro++;
+                this.pomodoroTimerPause();
+                this.setUpPomodoroTimer();
+            },
 
-    function skipToNextPomodoro() {
-        currentPomodoro++;
-        pausePomodoroTimer();
-        setupTimer();
-    }
+            getTimeFormat () {
+                let minutes = Math.floor(this.timeLeftInSeconds / 60);
+                let seconds = this.timeLeftInSeconds % 60;
 
-    function updatePomodoroTimer () {
-        pomodoroTimerText.textContent = getTimeFormat();
-    }
-
-    function startPomodoroTimer () {
-        timerInterval = setInterval(() => {
-            if (timeLeftInSeconds <= 0) {
-                clearInterval(timerInterval);
-                currentPomodoro++;
-                setupTimer();
-            } else {
-                timeLeftInSeconds--;
-                updatePomodoroTimer();
-            }
-        }, 1000);
-    }
-
-    function pausePomodoroTimer () {
-        clearInterval(timerInterval);
-    }
-
-    function getTimeFormat () {
-        let minutes = Math.floor(timeLeftInSeconds / 60);
-        let seconds = timeLeftInSeconds % 60;
-
-        let response = ""
-        if (minutes < 10) {
-            response += '0';
-        }
-        response += minutes.toString() + ":";
-        if (seconds < 10) {
-            response += '0';
-        }
-        response += seconds.toString();
-        return response;
-    }
-
-    startPomodoroTimerButton.addEventListener('click', startPomodoroTimer);
-    pausePomodoroTimerButton.addEventListener('click', pausePomodoroTimer);
-    skipToNextPomodoroButton.addEventListener('click', skipToNextPomodoro);
+                let res = ""
+                if (minutes < 10) {
+                    res += '0';
+                }
+                res += minutes.toString() + ":";
+                if (seconds < 10) {
+                    res += '0';
+                }
+                res += seconds.toString();
+                return res;
+            },
+        }));
+    });
 </script>
