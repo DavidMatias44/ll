126681478025aed8e77df8ff7c0e7a5c26f29014
DavidMatias44 <daosmaru3667@gmail.com>
2025-08-28 13:28:40 -0600

    refactor Pomodoro task selector view and livewire component

--- app/Livewire/PomodoroTaskSelector.php
+++ app/Livewire/PomodoroTaskSelector.php
@@ -3,41 +3,62 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Enums\Tasks\State;
 use App\Models\Task;
 use Auth;
 
 class PomodoroTaskSelector extends Component
 {
     public $tasks = null;
-    public $taskId;
-    public $task;
+    public $taskId = null;
+    public $task = null;
 
     public function mount()
     {
-        $userId = Auth::id();
-        $this->tasks = Task::query()->whereUserId($userId)->get();
-        
-        if (session('pomodoro.taskId') !== null) {
-            $this->taskId = session()->get('pomodoro.taskId');
-            $this->task = Task::findOrFail($this->taskId);
+        $this->loadTasks();
+        $this->loadSelectedTask();
+    }
+
+    public function loadTasks()
+    {
+        $this->tasks = Task::query()
+            ->whereUserId(Auth::id())
+            ->where('state', '<', State::COMPLETED)
+            ->get();
+    }
+
+    public function loadSelectedTask()
+    {  
+        $taskId = session()->get('pomodoro.taskId');
+        if ($taskId) {
+            $this->task = Task::findOrFail($taskId);
         }
     }
 
     public function saveTaskId() 
     {
+        /*
+            At this point $this->taskId is not null.
+            The button calling this function only appears when ($this->taskId != null).
+        */
         session()->put('pomodoro.taskId', $this->taskId);
-        $this->task = Task::findOrFail($this->taskId);
+        $this->loadSelectedTask();
+        $this->resetTaskId();
+    }
+
+    public function resetTaskId()
+    {
+        $this->taskId = null;
+    }
+
+    public function removeTask()
+    {
+        session()->forget('pomodoro.taskId');
     }
 
     public function updateTaskId()
     {
         return;
-    }
-
-    public function removeTask()
-    {
-        session()->put('pomodoro.taskId', null);
-        $this->taskId = null;
     }
 
     public function render()
--- resources/views/livewire/pomodoro-task-selector.blade.php
+++ resources/views/livewire/pomodoro-task-selector.blade.php
@@ -51,6 +51,10 @@
             <button @click="$dispatch('open-modal', 'show-tasks-modal')" x-data="" class="text-2xl bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-4 px-4 rounded">
                 Change task
             </button>
+
+            <button wire:click="removeTask" @click="$dispatch('close-modal', 'show-tasks-modal')" class="text-2xl bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-4 px-4 rounded">
+                Remove task
+            </button>
         @else
             @if (! $tasks->isEmpty())
                 <button @click="$dispatch('open-modal', 'show-tasks-modal')" x-data="" class="text-2xl bg-gray-600 hover:bg-gray-700 text-gray-800 dark:text-gray-100 py-4 px-4 rounded">
@@ -73,17 +77,19 @@
                     <select wire:model="taskId" wire:click="updateTaskId" class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                         <option value="null" selected disabled>Choose a task</option>
                         @foreach ($tasks as $task)
-                            <option value="{{ $task->id }}" @if ($task->id == $taskId) selected @endif>
+                            @if (session()->has('pomodoro.taskId') && session()->get('pomodoro.taskId') == $task->id)
+                                @continue
+                            @endif
+
+                            <option value="{{ $task->id }}">
                                 {{ $task->name }} | {{ $task->state->label() }} state | {{ $task->priority->label() }} priority</p>
                             </option>
                         @endforeach
                     </select>
 
-                    <x-secondary-button type="reset" @click="$dispatch('close-modal', 'show-tasks-modal')" class="mt-6">Cancel</x-secondary-button>
+                    <x-secondary-button type="reset" wire:click="resetTaskId" @click="$dispatch('close-modal', 'show-tasks-modal')" class="mt-6">Cancel</x-secondary-button>
 
-                    @if (session()->has('pomodoro.taskId') && session()->get('pomodoro.taskId') == $taskId)
-                        <x-primary-button type="button" wire:click="removeTask" @click="$dispatch('close-modal', 'show-tasks-modal')">Remove task</x-primary-button>
-                    @else
+                    @if ($taskId !== null)
                         <x-primary-button type="button" wire:click="saveTaskId" @click="$dispatch('close-modal', 'show-tasks-modal')">Select task</x-primary-button>
                     @endif
                 </form>
